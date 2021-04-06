f9f2af1e0fc372a3c1b1b00419090d75d74a0eef
Matheus Vieira <matheusvr423@gmail.com>
2021-04-05 22:46:11 -0300

    Adicionar arquivos de projeto.

---
+++ CSharpPOO/Aluno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace CSharpPOO
+{
+   public class Aluno
+    {
+        public string Nome;
+        public double Nota1,Nota2,Nota3;
+
+        public double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+        public override string ToString()
+        {
+          return  "NOTA FINAL = " +
+         NotaFinal().ToString("F2", CultureInfo.InvariantCulture); 
+        }
+
+
+
+
+    }
+    
+}
---
+++ CSharpPOO/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+namespace CSharpPOO
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno al = new Aluno();
+            
+            Console.Write("Nome do aluno: ");
+            al.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno: ");
+            al.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            al.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            al.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(al);
+
+            if (al.NotaFinal()< 60.00)
+            {
+                double diferenca = 60.00 - al.NotaFinal();
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM "+ diferenca.ToString("F2",
+                    CultureInfo.InvariantCulture)+ " PONTOS");
+            }
+            else
+            {
+                Console.WriteLine("APROVADO");
+            }
+
+        }
+    }
+}
